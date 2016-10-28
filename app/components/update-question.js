080ed41e583ac951019649b1b3e1360307e8c4a6
Stephen Burden <spburden@hotmail.com>
2016-10-28 10:10:17 -0700

    add update question component with routing

--- app/components/update-question.js
+++ app/components/update-question.js
@@ -1,4 +1,22 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  updateQuestionForm: false,
+  actions: {
+    updateFormShow() {
+      this.set('updateQuestionForm', true);
+    },
+    cancel() {
+       this.set('updateQuestionForm', false);
+    },
+    updateQuestion(question) {
+      var params = {
+        details: this.get('details'),
+        notes: this.get('notes'),
+        author: this.get('author')
+      };
+      this.set('updateQuestionForm', false);
+      this.sendAction('updateQuestion', question, params);
+    }
+  }
 });
