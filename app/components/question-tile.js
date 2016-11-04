33ba06ce590ad11d9d0e22bb906387cfc441316c
Stephen Burden <spburden@hotmail.com>
2016-11-04 10:06:40 -0700

    add service to collect favorite questions

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -1,6 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  qaCart: Ember.inject.service(),
+
   sortDefinition: ['answers.length:desc'],
   sortedQuestions: Ember.computed.sort('model.questions', 'sortDefinition'),
 
@@ -8,5 +10,9 @@ export default Ember.Component.extend({
     sortFilter(params){
       this.set('sortDefinition', [params]);
     },
+    faveQuestion(question){
+      this.get('qaCart').add(question);
+      console.log(this.get('qaCart.questions'));
+    }
   }
 });
