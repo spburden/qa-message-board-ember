f8eaf916b7b19fbf08f4ca125644ee571942e575
Stephen Burden <spburden@hotmail.com>
2016-11-04 08:59:59 -0700

    intergrate answer quantity and helper with more glyphicons onto home page

--- app/helpers/answer-quantity.js
+++ app/helpers/answer-quantity.js
@@ -3,10 +3,12 @@ import Ember from 'ember';
 export function answerQuantity(params/*, hash*/) {
   var quantity = params[0];
   console.log(quantity);
-  if(quantity === 1) {
-    return Ember.String.htmlSafe('<span> There is only 1 answer so far!</span>');
+  if(quantity === 0){
+    return Ember.String.htmlSafe('<span> There are no answers! <i class="fa fa-frown-o"></i></span>');
+  }else if(quantity === 1) {
+    return Ember.String.htmlSafe('<span> There is only 1 answer so far! <i class="fa fa-meh-o"></i></span>');
   } else if (quantity <= 3) {
-    return Ember.String.htmlSafe('<span> There are ' + quantity + ' answers </span>');
+    return Ember.String.htmlSafe('<span> There are ' + quantity + ' answers <i class="fa fa-smile-o"></i></span>');
   } else {
     return Ember.String.htmlSafe('<span> There are ' + quantity + ' answers <span class="glyphicon glyphicon-fire"></span> </span>');
   }
